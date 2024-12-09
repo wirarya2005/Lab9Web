cbb992f9ac3a9638842609be54f4754fdacc89ae
wirarya2005 <wirarya.tjoa@gmail.com>
2024-12-09 19:23:42 +0700

    file

---
+++ lab9_php_modular/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php require('header.php');?>
+    
+    <div class="content">
+        <h2>Ini halaman home</h2>
+        <p>Ini adalah bagian content dari halaman</p>
+    </div>
+
+    <?php require('footer.phsp');?>
+</body>
+</html>
---
+++ lab9_php_modular/index.php
@@ -0,0 +1,55 @@
+<?php
+include("koneksi.php");
+$sql = 'SELECT * FROM data_barang';
+$result = mysqli_query($conn, $sql);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Barang</title>
+    <link rel="stylesheet" href="style/app.css">
+
+</head>
+
+<body>
+    <div class="container">
+        <h1>Data Barang</h1>
+        <a href="tambah.php">Tambah Barang</a>
+        <div class="main">
+            <table>
+                <tr>
+                    <th>Gambar</th>
+                    <th>Nama Barang</th>
+                    <th>Katagori</th>
+                    <th>Harga Jual</th>
+                    <th>Harga Beli</th>
+                    <th>Stok</th>
+                    <th colspan="2">Aksi</th>
+                </tr>
+                <?php if ($result): ?>
+                    <?php while ($row = mysqli_fetch_array($result)): ?>
+                        <tr>
+                            <td><img src="gambar/<?= $row['gambar']; ?>" alt="" <?=
+                                  $row['nama']; ?>" class="foto-hp"></td>
+                            <td><?= $row['nama']; ?></td>
+                            <td><?= $row['kategori']; ?></td>
+                            <td><?= $row['harga_beli']; ?></td>
+                            <td><?= $row['harga_jual']; ?></td>
+                            <td><?= $row['stok']; ?></td>
+                            <td><a href="ubah.php?id=<?= $row['id_barang']; ?>">Ubah</a></td>
+                            <td><a href="hapus.php?id=<?= $row['id_barang']; ?>">Hapus</a></td>
+                        </tr>
+                    <?php endwhile; else: ?>
+                    <tr>
+                        <td colspan="7">Belum ada data</td>
+                    </tr>
+                <?php endif; ?>
+            </table>
+        </div>
+    </div>
+</body>
+
+</html>
